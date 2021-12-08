440903071220c42ad2c199dc273deb3f731e335e
davidsquines <davidsquines@gmail.com>
2021-12-08 13:51:56 -0800

    Changed query

--- controllers/viewControllers/getPositiveBlogsByUser.js
+++ controllers/viewControllers/getPositiveBlogsByUser.js
@@ -7,7 +7,7 @@ exports.getPositiveBlogsByUser = async(req, res, next) => {
         return res.status(422).json({errors: errors.array()});
     }
     try{
-        var query = "SELECT * FROM `blogs` as b where 0 = (SELECT COUNT(*) from `comments` as c where c.blogID = b.blogID and c.sentiment = 'Negative') and b.created_by = ?";
+        var query = "SELECT * FROM `blogs` as b where 0 = (SELECT COUNT(*) from `comments` as c where c.blogID = b.blogID and c.sentiment = 'Negative') and b.created_by = ? and b.blogID in (select blogID from comments)";
         var values = [req.body.username];
         const[rows] = await conn.execute(query, values);
        
